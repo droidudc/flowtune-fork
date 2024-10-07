e492e118059edb5f294e1ff866d4e0e25c3ef269
Abhiram <droid.udc@gmail.com>
2024-10-07 01:59:41 -0700

    Add files via upload

---
+++ material-color-utilities/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("java-library")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    compileOnly("com.google.errorprone:error_prone_core:2.31.0")
+}
